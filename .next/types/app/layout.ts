e53e124cdff2aa2a437db920d25b50360e0bf34c
Nory Pary <Paeypary@gmail.com>
2026-01-30 23:34:16 +0700

    deploy with k8s

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\NextJs Project\scanprize-web\src\app\layout.tsx
+// File: C:\PIIK_MAL\v3\scan_web\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\NextJs Project\scanprize-web\src\app\page.tsx
+// File: C:\PIIK_MAL\v3\scan_web\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
